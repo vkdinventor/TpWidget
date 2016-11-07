f17da7139f85c6b7a0476bc7be8807333bc28624
vikashkumar.verma <vikashkumar.verma@einfochips.com>
2016-11-07 18:32:49 +0530

    HotspotManager class is added

---
+++ app/src/main/java/com/example/vikash/tpwidget/HotspotManager.java
@@ -0,0 +1,38 @@
+package com.example.vikash.tpwidget;
+
+/**
+ * Created by vikash on 7/11/16.
+ */
+public class HotspotManager {
+
+    //check whether wifi hotspot on or off
+    public static boolean isApOn(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        try {
+            Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
+            method.setAccessible(true);
+            return (Boolean) method.invoke(wifimanager);
+        }
+        catch (Throwable ignored) {}
+        return false;
+    }
+
+    // toggle wifi hotspot on or off
+    public static boolean configApState(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        WifiConfiguration wificonfiguration = null;
+        try {
+            // if hotspot is on, turn it off
+            if(isApOn(context)) {
+                wifimanager.setWifiEnabled(false);
+            }
+            Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifimanager, wificonfiguration, !isApOn(context));
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+} // end of class
--- app/src/main/java/com/example/vikash/tpwidget/MainActivity.java
+++ app/src/main/java/com/example/vikash/tpwidget/MainActivity.java
@@ -6,11 +6,21 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.net.wifi.WifiManager;
 import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
+import java.lang.reflect.Method;
+import java.util.Random;
+
 public class MainActivity extends AppWidgetProvider{
+
+    String TAG="com.example.vikash.tpwidget";
+    WifiManager mWifiManager;
+    public static boolean status;
+    boolean hotspot;
+    public static int j=0;
 
     @Override
     public void onEnabled(Context context) {
@@ -19,23 +29,45 @@ public class MainActivity extends AppWidgetProvider{
     }
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        mWifiManager=(WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         for(int i=0; i<appWidgetIds.length; i++){
-            int currentWidgetId = appWidgetIds[i];
-            String url = "http://stackoverflow.com";
+            int widgetId = appWidgetIds[i];
+            String number = String.format("%03d", (new Random().nextInt(900) + 100));
 
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setData(Uri.parse(url));
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.activity_main);
+            HotspotManager.configApState(context); // change Ap state :boolean
 
-            PendingIntent pending = PendingIntent.getActivity(context, 0,intent, 0);
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_main);
+            remoteViews.setTextViewText(R.id.text, number+" "+HotspotManager.isApOn(context));
 
-            long time=System.currentTimeMillis()/1000;
-            views.setTextViewText(R.id.text," "+time);
-            Log.v("TAG", " "+time);
-            views.setOnClickPendingIntent(R.id.button, pending);
-            appWidgetManager.updateAppWidget(currentWidgetId,views);
-            Toast.makeText(context, "widget added", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, MainActivity.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context,
+                    0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.button, pendingIntent);
+            appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
         }
+    }
+
+    public boolean startHotSpot(boolean enable) {
+
+        mWifiManager.setWifiEnabled(false);
+        Method[] mMethods = mWifiManager.getClass().getDeclaredMethods();
+        for (Method mMethod : mMethods) {
+            if (mMethod.getName().equals("setWifiApEnabled")) {
+                try {
+                    mMethod.invoke(mWifiManager, null, enable);
+                    Log.v(TAG,"hotspot enabled");
+                    return true;
+                } catch (Exception ex) {
+                    Log.v(TAG,"Excpetion: "+ ex.toString());
+                }
+                break;
+            }
+            Log.v(TAG,"setWifiApEnabled method : "+mMethod.toString());
+        }
+        return false;
     }
 }
